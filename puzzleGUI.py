41d7907ac65e03bfe04d79dfc7578810d3a055a8
Pedro Barbosa <78980796+pedrobarbosaocb@users.noreply.github.com>
2023-09-25 16:48:56 -0300

    Refactor puzzleGUI.py

--- puzzleGUI.py
+++ puzzleGUI.py
@@ -2,97 +2,104 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import testeGrafo as tg
 
-# Lista de 9 imagens (substitua esses caminhos pelos caminhos reais das suas imagens)
-imagens = [
-    "./s/imagem_1.png",
-    "./_temp/imagem_2.png",
-    "./_temp/imagem_3.png",
-    "./_temp/imagem_4.png",
-    "./_temp/imagem_5.png",
-    "./_temp/imagem_6.png",
-    "./_temp/imagem_7.png",
-    "./_temp/imagem_8.png",
-    "./_temp/imagem_9.png"
-]
+class JogoPuzzleInterface:
+    def __init__(self):
+        self.janela = tk.Tk()
+        self.janela.title("Interface com 9 Imagens")
+        self.frame = tk.Frame(self.janela)
+        self.frame.pack()
 
-# Lista que representa a ordem inicial das imagens
-jogo = tg.PuzzleDoJogo()
-ordem_imagens = jogo.estadoInicial
+        # Lista de 9 imagens (substitua esses caminhos pelos caminhos reais das suas imagens)
+        self.imagens = [
+            "./src/imagem_1.png",
+            "./src/imagem_2.png",
+            "./src/imagem_3.png",
+            "./src/imagem_4.png",
+            "./src/imagem_5.png",
+            "./src/imagem_6.png",
+            "./src/imagem_7.png",
+            "./src/imagem_8.png",
+            "./src/imagem_9.png"
+        ]
 
-# Posição da imagem 0
-posicao_imagem_0 = (0, 0)
+        # Lista que representa a ordem inicial das imagens
+        self.jogo = tg.PuzzleDoJogo()
+        self.ordem_imagens = self.jogo.estadoInicial
 
-def criar_interface():
-    janela = tk.Tk()
-    janela.title("Interface com 9 Imagens")
+        self.imagem_widgets = {}
+        self.criar_interface(self.ordem_imagens)
 
-    imagem_widgets = {}
-    
-    for i in range(3):
-        for j in range(3):
-            posicao = ordem_imagens[i][j]
-            if posicao == 0:
-                continue
-            caminho_imagem = imagens[posicao - 1]
+    def criar_interface(self, ordem_atual):
+        for i in range(3):
+            for j in range(3):
+                posicao = ordem_atual[i][j]
+                if posicao == 0:
+                    continue
+                caminho_imagem = self.imagens[posicao - 1]
 
-            imagem = Image.open(caminho_imagem)
-            imagem = imagem.resize((100, 100), Image.LANCZOS)
-            imagem = ImageTk.PhotoImage(imagem)
-            
-            imagem_widget = tk.Label(janela, image=imagem)
-            imagem_widget.image = imagem
-            imagem_widget.grid(row=i, column=j, padx=5, pady=5)
+                imagem = Image.open(caminho_imagem)
+                imagem = imagem.resize((100, 100), Image.LANCZOS)
+                imagem = ImageTk.PhotoImage(imagem)
+                
+                imagem_widget = tk.Label(self.frame, image=imagem)
+                imagem_widget.image = imagem
+                imagem_widget.grid(row=i, column=j, padx=5, pady=5)
 
-            imagem_widgets[posicao] = imagem_widget
+                self.imagem_widgets[posicao] = imagem_widget
 
-    def clicar_imagem(event, estadoAtual=ordem_imagens, janela=janela):
+        for widget in self.imagem_widgets.values():
+            widget.bind("<Button-1>", self.clicar_imagem)
+
+    def clicar_imagem(self, event):
         posicao_imagem_0 = [0, 0]
 
-        for i in range(len(estadoAtual)):
-            for j in range(len(estadoAtual[0])):
-                if estadoAtual[i][j] == 0:
+        for i in range(len(self.ordem_imagens)):
+            for j in range(len(self.ordem_imagens[0])):
+                if self.ordem_imagens[i][j] == 0:
                     posicao_imagem_0[0] = i
                     posicao_imagem_0[1] = j
                     break
         
-        # print(posicao_imagem_0[0], posicao_imagem_0[1])
-
-        for posicao, widget in imagem_widgets.items():
+        for posicao, widget in self.imagem_widgets.items():
             if widget == event.widget:
-                global ordem_imagens
                 currentRow, currentCollumn = widget.grid_info()['row'], widget.grid_info()['column']
 
                 if currentRow == posicao_imagem_0[0] - 1 and currentCollumn == posicao_imagem_0[1]:
                     print("Cima")
-                    jogo.troca_posicao("cima")
+                    self.jogo.troca_posicao("cima")
                 elif currentRow == posicao_imagem_0[0] + 1 and currentCollumn == posicao_imagem_0[1]:
                     print("Baixo")
-                    jogo.troca_posicao("baixo")
+                    self.jogo.troca_posicao("baixo")
 
                 elif currentRow == posicao_imagem_0[0] and currentCollumn == posicao_imagem_0[1] - 1:
                     print("Esquerda")
-                    jogo.troca_posicao("esquerda")
+                    self.jogo.troca_posicao("esquerda")
                 elif currentRow == posicao_imagem_0[0] and currentCollumn == posicao_imagem_0[1] + 1:
                     print("Direita")
-                    jogo.troca_posicao("direita")
+                    self.jogo.troca_posicao("direita")
                 else:
                     break
-                ordem_imagens = jogo.estadoInicial
-                atualizar_ordem(janela)
-                break
+                self.ordem_imagens = self.jogo.estadoInicial
+                self.atualizar_ordem()
 
-    for widget in imagem_widgets.values():
-        widget.bind("<Button-1>", clicar_imagem)
+    def atualizar_ordem(self):
+        global ordem_imagens
+        ordem_imagens = self.jogo.estadoInicial
+        for posicao, widget in self.imagem_widgets.items():
+            widget.grid_forget()  # Remove o widget da grid
+            # Recria o widget com a nova posição
+            i, j = self.encontrar_posicao_widget(posicao)
+            widget.grid(row=i, column=j, padx=5, pady=5)
 
-    janela.mainloop()
+    def encontrar_posicao_widget(self, posicao):
+        for i in range(3):
+            for j in range(3):
+                if self.ordem_imagens[i][j] == posicao:
+                    return i, j
 
-def atualizar_ordem(janela):
-    global ordem_imagens, posicao_imagem_0
-    posicao_imagem_0 = (0, 0)
-    # janela.update()
-    # janela.update_idletasks()
-    janela.destroy()
-    criar_interface()
+    def executar(self):
+        self.janela.mainloop()
 
 if __name__ == "__main__":
-    criar_interface()
+    app = JogoPuzzleInterface()
+    app.executar()
